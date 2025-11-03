7db5d88b52f88436c274403bb0c6988a422928dd
Mert <espawnn@gmail.com>
2025-11-04 01:45:05 +0300

    Project v0.8

--- src/components/mainBody.js
+++ src/components/mainBody.js
@@ -1,8 +1,4 @@
-import RestoranRezervasyon from "../images/restoranRezerve.PNG";
-import KelimeBulmaca from "../images/kelimeBulmacaImg.PNG";
-import HesaplamaSitesi from "../images/hesaplamaSitesi.PNG";
-import ExampleEcommerce from "../images/exampleEcommerceSite.PNG";
-import TestYourLuck from "../images/testYourLuckImg.PNG"
+import { myWorksList } from "../utils/myWorks.js";
 
 function MyWorks() {
 
@@ -10,46 +6,20 @@ function MyWorks() {
     <div className="main-works">
       <h2>My Works</h2>
       <div className="works-list">
-        <div className="work-item test-your-luck">
-          <div className="work-item-text">
-            <a href="https://navillan.github.io/testYourLuck/" target="_blank" rel="noopener noreferrer">Test Your Luck</a>
-            <p>This was my first project; a game in which you compete against the computer. It marked the beginning of my journey. <a href="https://github.com/navillan/testYourLuck" target="_blank" rel="noopener noreferrer">Here</a> you can find the gitHub repo for this project.</p>
-            <p className="tech-stack-description">Techs: HTML5, CSS3, Vanilla JavaScript</p>
+        {myWorksList.map((work)=>(
+          <div className="work-item" key={work.name}>
+            <a className="work-name" href={work.live} target="_blank" rel="noopener noreferrer">{work.name}</a>
+            <p className="work-description">{work.description}
+              {" "}<a href={work.githubRepo}>Here</a>{" "}
+              you can find the gitHub repo for this project.
+              {(work.githubRepo2)
+              ?<p><a href="https://github.com/navillan/js990project" target="_blank" rel="noopener noreferrer">Here</a> you can find the same project with it's own backend.</p>
+              : null}
+            </p>            
+            <p className="tech-stack-description">Techs: {work.techs}</p>
+            <div className="work-item-image" style={{backgroundImage: `url(${work.image})`}}></div>
           </div>
-          <img className="work-item-image test-your-luck-img" src={TestYourLuck} alt="Test-Your-Luck-Screenshot" />
-        </div>
-        <div className="work-item restoran-rezervasyon">
-          <div className="work-item-text">
-            <a href="https://navillan.github.io/js990projectFirebase/" target="_blank" rel="noopener noreferrer">Restoran Rezervasyon Sitesi</a>
-            <p>A restaurant reservation website with firebase as it's backend. <a href="https://github.com/navillan/js990projectFirestore" target="_blank" rel="noopener noreferrer">Here</a> you can find the github repo for this project and <a href="https://github.com/navillan/js990project" target="_blank" rel="noopener noreferrer">here</a> you can find the same project with it's own backend.</p>
-            <p className="tech-stack-description">Techs(SPA): React, Firebase Techs(Backend): Node.js, CORS, Express, Firebase</p>
-          </div>
-          <img className="work-item-image restoran-rezervasyon-img" src={RestoranRezervasyon} alt="Restoran-Rezervasyon-Screenshot" />
-        </div>
-        <div className="work-item kelime-bulmaca">
-          <div className="work-item-text">
-            <a href="https://navillan.github.io/kelimeBulmaca/" target="_blank" rel="noopener noreferrer">Kelime Bulmaca</a>
-            <p>A word puzzle game for Turkish words. <a href="https://github.com/navillan/kelimeBulmaca" target="_blank" rel="noopener noreferrer">Here</a> you can find the github repo for this project.</p>
-            <p className="tech-stack-description">Techs: React, jQuery, Firebase</p>
-          </div>
-          <img className="work-item-image kelime-bulmaca-img" src={KelimeBulmaca} alt="Kelime-Bulmaca-Screenshot" />
-        </div>
-        <div className="work-item hesaplama-sitesi">
-          <div className="work-item-text">
-            <a href="https://navillan.github.io/hesaplamaSitesi" target="_blank" rel="noopener noreferrer">Hesap Sitesi</a>
-            <p>A calculator website offering a wide range of features. The project remains under active development; however, several capabilities are already available for use. <a href="https://github.com/navillan/hesaplamaSitesi" target="_blank" rel="noopener noreferrer">Here</a> you can find the github repo for this project.</p>
-            <p className="tech-stack-description">Techs: React</p>
-            <img className="work-item-image hesap-sitesi-img" src={HesaplamaSitesi} alt="Hesap-Sitesi-Screenshot" />
-          </div>
-        </div>
-        <div className="work-item example-ecommerce">
-          <div className="work-item-text">
-            <a href="https://navillan.github.io/exampleEcommerce" target="_blank" rel="noopener noreferrer">Example Ecommerce Site</a>
-            <p>An example ecommerce site. <a href="https://github.com/navillan/exampleEcommerce" target="_blank" rel="noopener noreferrer">Here</a> you can find the github repo for this project.</p>
-            <p className="tech-stack-description">Techs: React, React Router, jQuery</p>
-            <img className="work-item-image example-ecommerce-img" src={ExampleEcommerce} alt="Example-Ecommerce-Screenshot" />
-          </div>
-          </div>
+        ))}        
       </div>
     </div>
   );
---
+++ src/utils/myWorks.js
@@ -0,0 +1,49 @@
+import RestorantRezervasyon from "../images/restoranRezerve.PNG";
+import KelimeBulmaca from "../images/kelimeBulmacaImg.PNG";
+import HesaplamaSitesi from "../images/hesaplamaSitesi.PNG";
+import ExampleEcommerce from "../images/exampleEcommerceSite.PNG";
+import TestYourLuck from "../images/testYourLuckImg.PNG"
+
+export const myWorksList = [
+  {
+  name: "Test Your Luck",
+  description: "This was my first project; a game in which you compete against the computer. It marked the beginning of my journey.",
+  live: "https://navillan.github.io/testYourLuck/",
+  githubRepo: "https://github.com/navillan/testYourLuck",
+  techs: "HTML5, CSS3, Vanilla JavaScript",
+  image: TestYourLuck
+  }, 
+  {
+    name: "Restorant Rezervasyon Sitesi",
+    description: "A restaurant reservation website with firebase as it's backend.",
+    live: "https://navillan.github.io/js990projectFirebase/",
+    githubRepo: "https://github.com/navillan/js990projectFirestore",
+    githubRepo2: "https://github.com/navillan/js990project",
+    techs: "React, Firebase Techs(Backend): Node.js, CORS, Express, Firebase",
+    image: RestorantRezervasyon
+  },
+  {
+    name: "Kelime Bulmaca",
+    description: "A word puzzle game for Turkish words.",
+    live: "https://navillan.github.io/kelimeBulmaca/",
+    githubRepo: "https://github.com/navillan/kelimeBulmaca",
+    techs: "React, jQuery, Firebase",
+    image: KelimeBulmaca
+  },
+  {
+    name: "Hesap Sitesi",
+    description: "A calculator website offering a wide range of features. The project remains under active development; however, several capabilities are already available for use.",
+    live: "https://navillan.github.io/hesaplamaSitesi",
+    githubRepo: "https://github.com/navillan/hesaplamaSitesi",
+    techs: "React",
+    image: HesaplamaSitesi
+  },
+  {
+    name: "Example Ecommerce Site",
+    description: "An example ecommerce site.",
+    live: "https://navillan.github.io/exampleEcommerce",
+    githubRepo: "https://github.com/navillan/exampleEcommerce",
+    techs: "React, React Router, jQuery",
+    image: ExampleEcommerce
+  },
+]
