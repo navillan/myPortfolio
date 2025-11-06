5cc9e4056b25248a4b604de01dda061b5c42e70a
Mert <espawnn@gmail.com>
2025-11-06 18:16:30 +0300

    Project v0.8

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -5,19 +5,19 @@ function AboutMe() {
 
   return (
     <div className="about-me">
-      <h2>About Me</h2>
+      <h3>Hello! I'm Mert Selamlar, from Istanbul, Turkey.</h3>
       <div className="about-me-main">
         <img className="about-me-photo" src={ThisIsMe} alt="My-Picture" />
         <p className="about-me-text">
-          Hello! I'm Mert Selamlar, from Istanbul, Turkey. I have a degree
-          in English Language and Literature from <a href="https://www.linkedin.com/school/dogus-university" target="_blank" rel="noopener noreferrer">Doğus University</a>, which has helped me create deep connections with
-          people around the world and has made it easier for me to understand their cultures, which has greatly improved my communication skills. I'm a passionate developer with a
-          love for creating innovative solutions. I enjoy working with modern web 
-          technologies and continuously learning new skills to enhance my craft. I am 
-          enthusiastic about collaborating on exciting projects and contributing to the 
-          tech community. To enhance my skills, I have completed a comprehensive JavaScript
-          course from <a href="https://www.linkedin.com/company/aribilgi" target="_blank" rel="noopener noreferrer">Ari Bilgi Bilişim</a>, which has equipped me with a solid foundation in 
-          web development. I currently divide my time between developing personal projects and solving algorithm problems to prepare for a career in the industry. I'm eager to apply my knowledge and grow as a developer.
+          I am a Frontend Developer with strong communication skills and a solid command of the English language. I hold a degree in English Language and Literature from <a href="https://www.linkedin.com/school/dogus-university" target="_blank" rel="noopener noreferrer">Doğus University</a>, 
+          which has helped me create deep connections with people around the world and has made it easier for me to understand their cultures, 
+          which has greatly improved my communication skills. I'm a passionate developer with a love for creating innovative solutions. 
+          I enjoy working with modern web technologies and continuously learning new skills to enhance my craft. 
+          I am enthusiastic about collaborating on exciting projects and contributing to the tech community. 
+          To enhance my skills, I have completed a comprehensive JavaScript course from <a href="https://www.linkedin.com/company/aribilgi" target="_blank" rel="noopener noreferrer">Ari Bilgi Bilişim</a>, 
+          which has equipped me with a solid foundation in web development. 
+          I currently divide my time between developing personal projects and solving algorithm problems to prepare for a career in the industry. 
+          I'm eager to apply my knowledge and grow as a developer.
         </p>
       </div>
       <h2>My Javascript Certificate</h2>
--- src/components/mainContact.js
+++ src/components/mainContact.js
@@ -4,26 +4,24 @@ function Contact() {
   return (
     <div className="main-contact">
       <h2>Contact Me</h2>
-      <ul className="contact-list">
-        <li>
-          Email: 
+      <div className="contact-list">
+        <div className="contact-name">
+          <p>Email:</p>
+          <p>LinkedIn:</p>
+          <p>GitHub:</p>
+        </div>
+        <div className="contact-route">
           <a href="mailto:mertselamlar@gmail.com">
             mertselamlar@gmail.com
           </a>
-        </li>
-        <li>
-          LinkedIn: 
           <a href="https://linkedin.com/in/mert-selamlar-83709b140" target="_blank" rel="noopener noreferrer">
             linkedin.com/in/mert-selamlar-83709b140
           </a>
-        </li>
-        <li>
-          GitHub: 
           <a href="https://github.com/navillan" target="_blank" rel="noopener noreferrer">
             github.com/navillan
           </a>
-        </li>
-      </ul>      
+        </div>  
+      </div>    
     </div>
   );
 }
